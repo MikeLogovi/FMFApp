3f3e6b1698575d6560934c35468ca0857f0c2fb6
Mike LOGOVI <logovimike@gmail.com>
2019-03-25 21:45:44 +0100

    New commit

--- config/app.php
+++ config/app.php
@@ -164,6 +164,7 @@ return [
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
         MercurySeries\Flashy\FlashyServiceProvider::class,
+        LaravelPassportPassportServiceProvider::class,
         /*
          * Package Service Providers...
          */
